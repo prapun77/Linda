98237d6e6d77968cb6679a4eda2e83f388843ebd
prapun <39039851+prapun77@users.noreply.github.com>
2018-06-21 15:22:25 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -66,9 +66,9 @@ public:
         //    CTxIn(COutPoint(0000000000, 4294967295), coinbase 00012a24323020466562203230313420426974636f696e2041544d7320636f6d6520746f20555341)
         //    CTxOut(empty)
         //  vMerkleTree: 12630d16a9
-        const char* pszTimestamp = "Laptop ban on Etihad flights from Abu Dhabi lifted";
+        const char* pszTimestamp = "Ronaldo aging like 'port wine' at fourth World Cup";
         CTransaction txNew;
-        txNew.nTime = 1499037408;
+        txNew.nTime = 1529569224 ;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -77,7 +77,7 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime    = 1499037408;
+        genesis.nTime    = 1529569224 ;
         genesis.nBits    = bnProofOfWorkLimit.GetCompact();
         genesis.nNonce   = 7483051;
 

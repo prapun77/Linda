6fca8ec4ba8985c18fa446ba17434267b3d795ba
prapun77 <prapun77@gmail.com>
2018-06-21 22:24:24 +0700

    miner

--- src/miner.cpp
+++ src/miner.cpp
@@ -454,7 +454,7 @@ void FormatHashBuffers(CBlock* pblock, char* pmidstate, char* pdata, char* phash
 }
 
 
-bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
+bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey, bool search /* false */)
 {
     uint256 hashBlock = pblock->GetHash();
     uint256 hashProof = pblock->GetPoWHash();

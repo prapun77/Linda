3d637e7bb85410b3cac5398a78d85d2d92a8b792
prapun <39039851+prapun77@users.noreply.github.com>
2018-06-22 09:17:41 +0700

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,10 @@
 ./Lindad stop
 git pull
+mkdir /root/.Linda
+cp Linda.conf /root/.Linda/Linda.conf
+chmod +x compile.sh
 make -f makefile.unix
 strip Lindad
 cp Lindad /usr/local/bin
-mkdir /root/.Linda
-cp Linda.conf /root/.Linda
+
 Lindad -daemon
